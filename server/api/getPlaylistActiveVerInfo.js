084f946ec61740bd1b62a16ded2d24705c5d49ed
mike-3 <hauserm555@gmail.com>
2017-04-17 00:08:07 +0300

    refactor

---
+++ server/api/getPlaylistActiveVerInfo.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myPost_1 = require("../com/myPost");
+/**
+ * Created by Vlad on 4/16/2017.
+ */
+function getPlaylistActiveVerInfo(playlistId) {
+    var data = {
+        service: 'PremiumPlaylistService.getPlaylistActiveVerInfo',
+        playlistId: playlistId
+    };
+    return myPost_1.myPost(data).then(function (result) {
+        console.log(result);
+        var out = {
+            playlistId: result.response.responseClass[0].playlist_id[0],
+            versionId: result.response.responseClass[0].version_id[0]
+        };
+        return out;
+    }).catch(function (error) {
+        console.log(error);
+        return error;
+    });
+}
+exports.getPlaylistActiveVerInfo = getPlaylistActiveVerInfo;
+//# sourceMappingURL=getPlaylistActiveVerInfo.js.map
